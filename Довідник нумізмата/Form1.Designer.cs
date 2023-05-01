7963418823dc6059bf9dd9206a39521ff4bc51e2
IvanDukhota <129483315+IvanDukhota@users.noreply.github.com>
2023-05-01 16:51:22 +0300

    Update Form1.Designer.cs

--- Довідник нумізмата/Form1.Designer.cs
+++ Довідник нумізмата/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Довідник_нумізмата
+namespace Довідник_нумізмата
 {
     partial class Form1
     {
@@ -28,18 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            panel1 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(3, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(248, 26);
+            label1.TabIndex = 0;
+            label1.Text = " Довідник Нумізмата:";
+            label1.Click += label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1125, 42);
+            panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(12, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 26);
+            label2.TabIndex = 2;
+            label2.Text = "Монети:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(942, 77);
+            label3.Name = "label3";
+            label3.Size = new Size(171, 26);
+            label3.TabIndex = 3;
+            label3.Text = "Колекціонери:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 115);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(494, 471);
+            dataGridView1.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(419, 186);
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(1125, 715);
+            Controls.Add(dataGridView1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Label label2;
+        private Label label3;
+        private DataGridView dataGridView1;
     }
 }
