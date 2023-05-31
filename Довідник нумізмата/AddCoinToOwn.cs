30765aae51d40791df4e0560e2d7a3d0039c3235
IvanDukhota <129483315+IvanDukhota@users.noreply.github.com>
2023-05-31 21:22:00 +0300

    Update AddCoinToOwn.cs

--- Довідник нумізмата/AddCoinToOwn.cs
+++ Довідник нумізмата/AddCoinToOwn.cs
@@ -27,9 +27,59 @@ namespace Довідник_нумізмата
         {
             try
             {
+                string coinName = textBoxName.Text;
+                string coinCountry = textBoxCountry.Text;
+                string coinDenomination = textBoxDenomination.Text;
+                string coinMetal = textBoxMetal.Text;
+                string coinFeature = textBoxFeature.Text;
 
-                OwnCollectionList.Add(new Classes.OwnCollection(textBoxName.Text, textBoxCountry.Text, Convert.ToInt32(textBoxYear.Text), Convert.ToInt32(textBoxAmount.Text), textBoxDenomination.Text, textBoxMetal.Text, textBoxFeature.Text));
+                int coinYear;
+                if (!int.TryParse(textBoxYear.Text, out coinYear))
+                {
+                    MessageBox.Show("Помилка: Введіть числове значення в поле 'Рік випуску'");
+                    return;
+                }
+
+                int coinAmount;
+                if (!int.TryParse(textBoxAmount.Text, out coinAmount))
+                {
+                    MessageBox.Show("Помилка: Введіть числове значення в поле 'Кількість випущених монет'");
+                    return;
+                }
+
+                if (coinName.Any(char.IsDigit))
+                {
+                    MessageBox.Show("Помилка: Введіть текстове значення в поле 'Ім'я'");
+                    return;
+                }
+
+                if (coinCountry.Any(char.IsDigit))
+                {
+                    MessageBox.Show("Помилка: Введіть текстове значення в поле 'Країна'");
+                    return;
+                }
+
+                if (coinDenomination.Any(char.IsDigit))
+                {
+                    MessageBox.Show("Помилка: Введіть текстове значення в поле 'Номінал'");
+                    return;
+                }
+
+                if (coinMetal.Any(char.IsDigit))
+                {
+                    MessageBox.Show("Помилка: Введіть текстове значення в поле 'Метал'");
+                    return;
+                }
+
+                if (coinFeature.Any(char.IsDigit))
+                {
+                    MessageBox.Show("Помилка: Введіть текстове значення в поле 'Особливість'");
+                    return;
+                }
+
+                OwnCollectionList.Add(new Classes.OwnCollection(coinName, coinCountry, coinYear, coinAmount, coinDenomination, coinMetal, coinFeature));
                 MessageBox.Show("Монета додана");
+
                 textBoxName.Text = "";
                 textBoxCountry.Text = "";
                 textBoxYear.Text = "";
@@ -37,13 +87,10 @@ namespace Довідник_нумізмата
                 textBoxDenomination.Text = "";
                 textBoxMetal.Text = "";
                 textBoxFeature.Text = "";
-
-
-
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Помилка " + ex.Message);
+                MessageBox.Show("Помилка: " + ex.Message);
             }
         }
         private void Form5_Load(object sender, EventArgs e)
